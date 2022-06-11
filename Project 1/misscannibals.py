980eb7cf6e8a06f61afa6278976d262b29fdf363
arif599 <arifullashaik01@gmail.com>
2022-06-10 18:37:45 -0700

    finished actions method

--- Project 1/misscannibals.py
+++ Project 1/misscannibals.py
@@ -7,8 +7,23 @@ class MissCannibals(Problem):
         self.C = C
         super().__init__(initial, goal)
 
-    # YOUR CODE GOES HERE
-	
+    def actions(self, state):
+        """ Return the actions that can be executed in the given state.
+        The result would be a list, since there are only five possible actions ('MM', 'MC', 'CC', 'M', 'C')
+        in any given state of the environment """
+
+        possible_actions = ['MM', 'MC', 'CC', 'M', 'C']
+        mCount = state[0]
+        cCount = state[1]
+
+        for action in possible_actions:
+            if action.count('M') > mCount or action.count('C') > cCount:
+                possible_actions.remove(action)
+
+        return possible_actions
+
+    
+
 if __name__ == '__main__':
     mc = MissCannibals(3,3)
     #print(mc.actions((3, 2, True))) # Test your code as you develop! This should return  ['CC', 'C', 'M']
